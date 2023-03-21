dcca230ff6954088d7847762f29142f6d1394a6f
Gavin-Swartz <gvndswartz@gmail.com>
2023-03-21 12:14:00 -0500

    Retrieves article text with search. Search needs to be modified.

--- wiki_search.py
+++ wiki_search.py
@@ -10,7 +10,7 @@ def find_site_links(soup):
 
 def get_page_title(link, page_titles, page_links):
     new_title = link.get('title')
-    if new_title not in page_titles and not str(new_title).endswith('(page does not exist)'):
+    if new_title not in page_titles and not str(new_title).endswith('(page does not exist)') and not str(new_title).startswith('Special:BookSources/'):
         new_link = link.get('href')
         new_link = 'https://en.wikipedia.org' + new_link
         page_links.append(new_link)
@@ -21,7 +21,7 @@ page_titles = []
 page_links = []
 
 # Computer article on Wikipedia
-url = 'https://en.wikipedia.org/wiki/Computer'
+url = 'https://en.wikipedia.org/wiki/computers'
 
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -33,7 +33,3 @@ for link in links:
 
 for title in page_titles:
     print(title)
-
-print(len(page_links))
-print(len(page_titles))
-
--- wiki_text.py
+++ wiki_text.py
@@ -1,20 +1,46 @@
+import difflib
+
 import requests
 from bs4 import BeautifulSoup
 import wiki_search
 
-def user_page_selection():
-    user_page_of_interest = input('Enter a page of interest: ')
 
-    if user_page_of_interest in wiki_search.page_titles:
-        print('Page found')
-        index = wiki_search.page_titles.index(user_page_of_interest)
+def user_page_search(page_titles):
+
+    user_search = input('Enter a topic you are interested in: ')
+
+    index = 0
+    while index < len(page_titles):
+        page_titles[index] = str(page_titles[index])
+        index += 1
+
+    return search(page_titles, user_search)
+
+
+def search(page_titles, user_search):
+    # Top 3 matches
+    closest_matches = difflib.get_close_matches(user_search, page_titles)
+    index = 0
+    while index < len(closest_matches):
+        display_number = index + 1
+        print(str(display_number) + '. ' + closest_matches[index])
+        index += 1
+    option = input('Which option best matches your search?')
+    if option == str(1):
+        index = page_titles.index(closest_matches[0])
+        return wiki_search.page_links[index]
+    elif option == 2:
+        index = page_titles.index(closest_matches[1])
+        return wiki_search.page_links[index]
+    elif option == 3:
+        index = page_titles.index(closest_matches[2])
         return wiki_search.page_links[index]
     else:
-        print('Enter a new value')
-        user_page_selection()
+        print('Please enter a valid integer')
+        search(page_titles, user_search)
 
 
-url = user_page_selection()
+url = user_page_search(wiki_search.page_titles)
 
 response = requests.get(url).text
 soup = BeautifulSoup(response, 'html.parser')
