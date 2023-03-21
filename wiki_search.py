e916a7794b8d75645aac493afae77e2a9c7fb581
Gavin-Swartz <gvndswartz@gmail.com>
2023-03-20 19:36:13 -0500

    Made search so an article can be searched for and the link can be retrieved.

--- wiki_search.py
+++ wiki_search.py
@@ -8,18 +8,29 @@ def find_site_links(soup):
     return links
 
 
-def get_link_urls(links, links_list):
-    for link in links:
+def get_page_title(link, page_titles, page_links):
+    new_title = link.get('title')
+    if new_title not in page_titles and not str(new_title).endswith('(page does not exist)'):
         new_link = link.get('href')
-        new_link = str(new_link)
-        if new_link.startswith('/wiki/') and new_link not in links_list:
-            new_link = 'https://en.wikipedia.org/' + new_link
-            links_list.append(new_link)
-    return links_list
+        new_link = 'https://en.wikipedia.org' + new_link
+        page_links.append(new_link)
+        page_titles.append(new_title)
 
 
+def user_page_selection():
+    user_page_of_interest = input('Enter a page of interest: ')
 
-links_list = []
+    if user_page_of_interest in page_titles:
+        print('Page found')
+        index = page_titles.index(user_page_of_interest)
+        print(page_links[index])
+    else:
+        print('Enter a new value')
+        user_page_selection()
+
+
+page_titles = []
+page_links = []
 
 # Computer article on Wikipedia
 url = 'https://en.wikipedia.org/wiki/Computer'
@@ -29,6 +40,13 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 links = find_site_links(soup)
 
-site_urls = get_link_urls(links, links_list)
-print(site_urls)
+for link in links:
+    get_page_title(link, page_titles, page_links)
 
+for title in page_titles:
+    print(title)
+
+print(len(page_links))
+print(len(page_titles))
+
+user_page_selection()
