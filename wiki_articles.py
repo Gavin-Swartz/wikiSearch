3a26a46371ebe128e41612b4ac309cc0fb46bb28
Gavin-Swartz <gvndswartz@gmail.com>
2023-03-21 13:10:28 -0500

    Allows user to pick from articles.

---
+++ wiki_articles.py
@@ -0,0 +1,23 @@
+# Things
+computer = 'https://en.wikipedia.org/wiki/computers'
+
+# People
+bill_gates = 'https://en.wikipedia.org/wiki/Bill_Gates'
+zuckerberg = 'https://en.wikipedia.org/wiki/Mark_Zuckerberg'
+einstein = 'https://en.wikipedia.org/wiki/Albert_Einstein'
+musk = 'https://en.wikipedia.org/wiki/Elon_Musk'
+
+# Places
+usa = 'https://en.wikipedia.org/wiki/United_States'
+earth = 'https://en.wikipedia.org/wiki/Earth'
+
+# Music
+nirvana = 'https://en.wikipedia.org/wiki/Nirvana_(band)'
+dark_side_of_the_moon = 'https://en.wikipedia.org/wiki/The_Dark_Side_of_the_Moon'
+
+# Books
+art_of_war = 'https://en.wikipedia.org/wiki/The_Art_of_War'
+laws_of_power = 'https://en.wikipedia.org/wiki/The_48_Laws_of_Power'
+
+# List of links
+links = [computer, bill_gates, zuckerberg, einstein, musk, usa, earth, nirvana, dark_side_of_the_moon, art_of_war, laws_of_power]
--- wiki_search.py
+++ wiki_search.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import wiki_articles as articles
 
 
 def find_site_links(soup):
@@ -10,20 +11,31 @@ def find_site_links(soup):
 
 def get_page_title(link, page_titles, page_links):
     new_title = link.get('title')
-    new_title = str(new_title)
-    if new_title not in page_titles and not new_title.endswith('(page does not exist)') and not new_title.startswith('Special:BookSources/') and new_title != 'None':
+    new_title = str(new_title).lower()
+    if new_title not in page_titles and not new_title.endswith('(page does not exist)') and\
+            not new_title.startswith('special:') and new_title != 'None':
         new_link = link.get('href')
-        new_link = str(new_link)
-        new_link = 'https://en.wikipedia.org' + new_link
-        page_links.append(new_link)
-        page_titles.append(new_title.lower())
+        if new_link is not None:
+            new_link = str(new_link)
+            new_link = 'https://en.wikipedia.org' + new_link
+            page_links.append(new_link)
+            page_titles.append(new_title)
+
+
+def choose_article():
+    index = 0
+    for article in articles.links:
+        print(str(index + 1) + '. ' + article)
+        index += 1
+    user_choice = int(input('Choose an article to pull links from: '))
+    url = articles.links[user_choice - 1]
+    return url
 
 
 page_titles = []
 page_links = []
 
-# Computer article on Wikipedia
-url = 'https://en.wikipedia.org/wiki/computers'
+url = choose_article()
 
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -35,3 +47,4 @@ for link in links:
 
 for title in page_titles:
     print(title)
+print(str(len(page_titles)) + ' results')
