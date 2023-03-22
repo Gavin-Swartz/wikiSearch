fb51ee6e4e43945bc0306a59566ce08526e9d0f0
Gavin-Swartz <gvndswartz@gmail.com>
2023-03-22 00:19:28 -0500

    Refactored files and methods.

--- wiki_crawler.py
+++ wiki_crawler.py
@@ -12,7 +12,7 @@ def find_site_links():
 def get_page_title():
     new_title = link.get('title')
     new_title = str(new_title).lower()
-    if new_title not in page_titles and not new_title.endswith('(page does not exist)') and\
+    if new_title not in page_titles and not new_title.endswith('(page does not exist)') and \
             not new_title.startswith('special:') and new_title != 'None':
         new_link = link.get('href')
         if new_link is not None:
@@ -48,10 +48,3 @@ links = find_site_links()
 
 for link in links:
     get_page_title()
-
-print_index = 0
-for title in page_titles:
-    print(str(print_index + 1) + '. ' + title)
-    print_index += 1
-
-print(str(len(page_titles)) + ' results')
--- wiki_text.py
+++ wiki_text.py
@@ -1,23 +1,34 @@
-import difflib
 import requests
 from bs4 import BeautifulSoup
-import wiki_search
+import wiki_crawler
+
+
+def print_titles(page_titles):
+    print_index = 0
+    for title in page_titles:
+        print(str(print_index + 1) + '. ' + title)
+        print_index += 1
+
+    print(str(len(page_titles)) + ' results')
 
 
 def user_page_search(page_links):
     user_search = input('Enter the number of the topic you are interested in: ')
-    selected_url = page_links[int(user_search) -1]
+    selected_url = page_links[int(user_search) - 1]
     return selected_url
 
 
-url = user_page_search(wiki_search.page_links)
+def get_text(soup):
+    text_list = []
+    for tag in soup.find_all('p'):
+        text_list.append(tag.text)
+    print(' '.join(text_list))
+
+
+print_titles(wiki_crawler.page_titles)
+url = user_page_search(wiki_crawler.page_links)
 
 response = requests.get(url).text
 soup = BeautifulSoup(response, 'html.parser')
 
-text_list = []
-
-for tag in soup.find_all('p'):
-    text_list.append(tag.text)
-
-print(' '.join(text_list))
+get_text(soup)
