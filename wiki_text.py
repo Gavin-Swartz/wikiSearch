7e81b3b18f28d52a15d222ff12056f869206e678
Gavin-Swartz <gvndswartz@gmail.com>
2023-03-22 00:24:02 -0500

    Print original page

--- wiki_text.py
+++ wiki_text.py
@@ -18,17 +18,22 @@ def user_page_search(page_links):
     return selected_url
 
 
-def get_text(soup):
+def response_and_beautifulsoup(url):
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, 'html.parser')
+    return soup
+
+
+def get_text(url):
     text_list = []
+    soup = response_and_beautifulsoup(url)
     for tag in soup.find_all('p'):
         text_list.append(tag.text)
     print(' '.join(text_list))
 
 
+get_text(wiki_crawler.url)
 print_titles(wiki_crawler.page_titles)
 url = user_page_search(wiki_crawler.page_links)
 
-response = requests.get(url).text
-soup = BeautifulSoup(response, 'html.parser')
-
-get_text(soup)
+get_text(url)
